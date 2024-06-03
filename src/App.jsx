26137c31bac3d5ee65977b40a6437a01b65147a6
Daniel <marin.danidm@gmail.com>
2024-06-03 23:41:17 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar';
@@ -8,13 +8,29 @@ import Navbar from './components/Navbar';
 
 function App() {
   const [sidebarToggle, setSidebarToggle] = useState(false);
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false)
+
+  const handleResize = () => {
+    if (window.innerWidth < 800) {
+      setIsNavbarVisible(true)
+    }
+    else {
+      setIsNavbarVisible(false)
+    }
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+  }, [])
 
   return (
     <>
       <div className="flex">
-        <Sidebar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} />
+        {
+          !isNavbarVisible ? <Sidebar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} /> : <Navbar />
+        }
         <div className={`flex-1 ${sidebarToggle ? 'ml-64' : 'ml-16'} transition-all duration-300 ease-in-out`}>
-          {/* <Navbar /> */}
           <Outlet />
         </div>
       </div>
