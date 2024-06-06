0e860b6221a1491460b850dc53585cb8d8780aea
Daniel <marin.danidm@gmail.com>
2024-06-06 14:00:37 +0300

    changes

---
+++ src/components/DropUpSearch.jsx
@@ -0,0 +1,3 @@
+export default function DropUpSearch() {
+    return <div>DROPWDOWN</div>
+} 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaHome, FaSearch, FaStoreAlt } from 'react-icons/fa';
 import { GiCellarBarrels } from 'react-icons/gi';
 import { LiaWineBottleSolid } from 'react-icons/lia';
 import { LuMenuSquare } from 'react-icons/lu';
 import { PiWineDuotone } from 'react-icons/pi';
 import { Link } from 'react-router-dom';
+import DropUpSearch from './DropUpSearch';
 
 export default function Sidebar() {
     const [isSidebarVisible, setIsSidebarVisible] = useState(false)
+    const [isDropdownActive, setIsDropdownActive] = useState(true)
+    const [searchValue, setSearchValue] = useState('')
+
+    useEffect(() => {
+        setIsDropdownActive(!searchValue ? false : true)
+    }, [searchValue])
+
     return (
         <nav
             aria-label="Sidebar"
@@ -20,49 +28,51 @@ export default function Sidebar() {
             onMouseOver={() => setIsSidebarVisible(true)}
             onMouseLeave={() => setIsSidebarVisible(false)}
         >
-            <div className="my-2 mb-4 flex items-center justify-center w-full">
+            <div className={`my-2 mb-4 flex items-center justify-center w-full ${!isDropdownActive ? 'block' : 'hidden'}`}>
                 <LuMenuSquare className="w-10 h-10 text-orange-300" />
                 <h1 className={`text-2xl text-white font-bold ml-2 ${isSidebarVisible ? 'block' : 'hidden'}`}>
                     CruRated
                 </h1>
             </div>
-            <ul className={`w-full flex-col items-center mt-3 text-white font-bold ${isSidebarVisible ? 'block' : 'hidden'}`}>
-                <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
-                    <a href="/" className="px-3 flex items-center w-full">
-                        <FaHome className="w-6 h-6 mr-2" />
-                        <span className="hidden group-hover:inline lg:inline">Home</span>
-                    </a>
-                </li>
-                <hr className="border-gray-600 w-full" />
-                <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
-                    <a href="/collections" className="px-3 flex items-center w-full">
-                        <LiaWineBottleSolid className="w-6 h-6 mr-2" />
-                        <span className="hidden group-hover:inline lg:inline">Collections</span>
-                    </a>
-                </li>
-                <hr className="border-gray-600 w-full" />
-                <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
-                    <a href="/barrels" className="px-3 flex items-center w-full">
-                        <GiCellarBarrels className="w-6 h-6 mr-2" />
-                        <span className="hidden group-hover:inline lg:inline">Barrels</span>
-                    </a>
-                </li>
-                <hr className="border-gray-600 w-full" />
-                <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
-                    <Link to="/shop/page/1" className="px-3 flex items-center w-full">
-                        <FaStoreAlt className="w-6 h-6 mr-2" />
-                        <span className="hidden group-hover:inline lg:inline">Shop</span>
-                    </Link>
-                </li>
-                <hr className="border-gray-600 w-full" />
-                <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
-                    <a href="/singleLots" className="px-3 flex items-center w-full">
-                        <PiWineDuotone className="w-6 h-6 mr-2" />
-                        <span className="hidden group-hover:inline lg:inline">Single Lots</span>
-                    </a>
-                </li>
-                <hr className="border-gray-600 w-full" />
-            </ul>
+            {isDropdownActive ? <span><DropUpSearch /></span> :
+                <ul className={`w-full flex-col items-center mt-3 text-white font-bold ${isSidebarVisible ? 'block' : 'hidden'}`}>
+                    <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
+                        <a href="/" className="px-3 flex items-center w-full">
+                            <FaHome className="w-6 h-6 mr-2" />
+                            <span className="hidden group-hover:inline lg:inline">Home</span>
+                        </a>
+                    </li>
+                    <hr className="border-gray-600 w-full" />
+                    <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
+                        <a href="/collections" className="px-3 flex items-center w-full">
+                            <LiaWineBottleSolid className="w-6 h-6 mr-2" />
+                            <span className="hidden group-hover:inline lg:inline">Collections</span>
+                        </a>
+                    </li>
+                    <hr className="border-gray-600 w-full" />
+                    <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
+                        <a href="/barrels" className="px-3 flex items-center w-full">
+                            <GiCellarBarrels className="w-6 h-6 mr-2" />
+                            <span className="hidden group-hover:inline lg:inline">Barrels</span>
+                        </a>
+                    </li>
+                    <hr className="border-gray-600 w-full" />
+                    <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
+                        <Link to="/shop/page/1" className="px-3 flex items-center w-full">
+                            <FaStoreAlt className="w-6 h-6 mr-2" />
+                            <span className="hidden group-hover:inline lg:inline">Shop</span>
+                        </Link>
+                    </li>
+                    <hr className="border-gray-600 w-full" />
+                    <li className="mb-2 rounded hover:shadow hover:text-orange-300 hover:bg-rose-900 py-2 w-full">
+                        <a href="/singleLots" className="px-3 flex items-center w-full">
+                            <PiWineDuotone className="w-6 h-6 mr-2" />
+                            <span className="hidden group-hover:inline lg:inline">Single Lots</span>
+                        </a>
+                    </li>
+                    <hr className="border-gray-600 w-full" />
+                </ul>
+            }
             <div className={`relative p-3 w-full flex items-center justify-center ${isSidebarVisible ? 'block' : 'hidden'}`}>
                 <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                     <FaSearch className="w-4 h-4 ml-3 text-gray-500" />
@@ -71,6 +81,8 @@ export default function Sidebar() {
                     type="text"
                     placeholder="Explore CruRated"
                     className="w-full px-4 py-2 pl-10 rounded bg-gray-800 text-white focus:outline-none"
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
                 />
             </div>
         </nav>
