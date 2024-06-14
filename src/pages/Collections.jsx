c38a9f3f9106c75c63b834e1a67f03222379dfee
Daniel <marin.danidm@gmail.com>
2024-06-14 17:22:23 +0300

    changes

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -47,7 +47,7 @@ export default function Collections() {
     return <div className="relative flex flex-col items-center">
         <img className="top-0 left-0 p-0 h-screen object-cover z-0 w-full" src={heroImg} />
         <div className="absolute flex flex-col items-center text-center m-10 lg:p-48">
-            <h1 className="text-3xl text-white lg:text-5xl mb-10 lg:mb-24 mt-2 outline-10 outline-black">EVERY BOTTLE HAS A DRINKING WINDOW. OURS HAS A BUYING WINDOW.</h1>
+            <h1 className="text-3xl text-white lg:text-5xl mb-10 lg:mb-24 mt-2 outline-10 outline-black opacity-80">EVERY BOTTLE HAS A DRINKING WINDOW. OURS HAS A BUYING WINDOW.</h1>
             <p className="text-l text-white lg:text-2xl mt-5 opacity-80 mb-14 lg:mb-40">Discover the most sought-after bottles, in our weekly online auctions.</p>
             <Button text="Explore Collections" />
         </div>
